6c1b61171315ddef6bd3ef34616dbb101e748a29
Jesus Enrique Silva Garcia <Silvak-@outlook.com>
2021-01-19 23:37:51 -0400

    w

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ def home():
     
 @app.route('/about')
 def about():
-    return 'Hello'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
