43f0b1a1180d9fd83d220da09a761146897b2849
Jesus Enrique Silva Garcia <Silvak-@outlook.com>
2021-01-20 20:15:35 -0400

    su

--- app.py
+++ app.py
@@ -1,44 +1,51 @@
 import os
 from flask import Flask, render_template, request, redirect
-from flask.helpers import flash
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/Users/Silvak/Desktop/Program/javascript/nova_imageUp/static/uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_PATH = '/Users/Silvak/Desktop/Program/javascript/nova_imageUp/static/img/uploads'
+ALLOWED_EXTENSIONS = {'PNG', 'JPG', 'JPEG', 'GIF'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = UPLOAD_PATH
+app.config['ALLOWED_IMAGE_EXTENSIONS'] = ALLOWED_EXTENSIONS
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def allow_image(filename):
+    if not '.' in filename:
+        return False
+    ext = filename.rsplit('.',1)[1]
+
+    if ext.upper() in app.config['ALLOWED_IMAGE_EXTENSIONS']:
+        return True
+    else:
+        return False
 
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def upload_image():
     if request.method == 'POST':
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-
         if request.files:
+
             image = request.files['image']
-            image.save(os.path.join(app.config['IMAGES_UPLOADS'], image.filename))
-            print('Image saved') # mandar valor en % a el template en ves del texto
-            print(image)
+
+            if image.filename == '':
+                print('image must have a filename')
+                return redirect(request.url)
+            
+            if not allow_image(image.filename):
+                print('that image extension is not allowd')
+                return redirect(request.url)
+
+            else:
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            print('image saved')
             return redirect(request.url)
 
-    return render_template('index.html', msm= 'aaaa')
+    return render_template('index.html')
+
 
 
 
